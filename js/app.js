95286d650ffa8cde24a1b61c5a55ab943907efba
Leland Jordon <lelandjordon@mac.com>
2016-05-31 15:13:59 -0400

    added pets folder

--- js/app.js
+++ js/app.js
@@ -1,4 +1,28 @@
-var hello = function(){
-  console.log("Hello, Dave");
-};
-hello();
+"use strict";
+
+(function(){
+  angular
+  .module("petseeker", [
+    "ui.router"
+  ])
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("petIndex", {
+      url: "/pets",
+      templateUrl: "js/pets/index.html"
+    })
+    .state("petShow", {
+      url: "/pets/:id",
+      templateUrl: "js/pets/show.html"
+    });
+  }
+
+
+
+
+})();
