586a199245f0212eaabe68374a7a451bbfab0322
Leland Jordon <lelandjordon@mac.com>
2016-06-22 09:23:38 -0400

    working on routes in app.js for deployment

--- js/app.js
+++ js/app.js
@@ -18,25 +18,25 @@
     $stateProvider
     .state("petIndex", {
       url:          "/pets",
-      templateUrl:  "js/pets/index.html",
+      templateUrl:  "pet-seeker/js/pets/index.html",
       controller:   "PetIndexController",
       controllerAs: "PetIndexViewModel"
     })
     .state("petNew", {
       url:          "/pets/new",
-      templateUrl:  "js/pets/new.html",
+      templateUrl:  "pet-seeker/js/pets/new.html",
       controller:   "PetNewController",
       controllerAs: "PetNewViewModel"
     })
     .state("petShow", {
       url:          "/pets/:id",
-      templateUrl:  "js/pets/show.html",
+      templateUrl:  "pet-seeker/js/pets/show.html",
       controller:   "PetShowController",
       controllerAs: "PetShowViewModel"
     })
     .state("petEdit", {
       url:          "/pets/:id/edit",
-      templateUrl:  "js/pets/edit.html",
+      templateUrl:  "pet-seeker/js/pets/edit.html",
       controller:   "PetEditController",
       controllerAs: "PetEditViewModel"
     });
